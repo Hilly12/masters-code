714f1e61cf45fe0acd534010dc1aba74b25a8dcf
Hilly12 <aahilmehta12@gmail.com>
2022-06-14 17:21:09 +0100

    Add pytest to setup.cfg

--- src/prifair/optimizers.py
+++ src/prifair/optimizers.py
@@ -1,6 +1,7 @@
 """Wrappers for optimizers that specific algorithms use during training. """
 
-from typing import List, Optional
+from abc import abstractmethod
+from typing import Any, Dict, List, Optional
 
 import torch
 from opacus.optimizers import DPOptimizer
@@ -13,19 +14,13 @@ from opacus.optimizers.optimizer import (  # _generate_noise,
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
-class DPSGDFOptimizer(DPOptimizer):
+class AdaptiveClippingOptimizer(DPOptimizer):
     """
-    Implementation of DPSGD-F from Xu et al. [1].
-
     A wrapper for ``torch.optim.Optimizer`` that adds additional functionality
     to clip per sample gradients and add Gaussian noise.
     Similar to ``opacus.optimizers.DPOptimizer``, but uses adaptive clipping thresholds.
 
-    Can be used with any ``torch.optim.Optimizer`` subclass as an underlying optimizer.
-    ``DPSGDFOptimizer`` assumes that parameters over which it performs optimization
-    belong to GradSampleModule and therefore have the ``grad_sample`` attribute.
-
-    On a high level ``DPSGDOptimizer``'s step looks like this:
+    On a high level ``AdaptiveClippingOptimizer``'s step looks like this:
     1) Aggregate ``p.grad_sample`` over all parameters to calculate per sample norms.
     2) Adaptively clip ``p.grad_sample`` so that the per sample norm is not above
     the computed threshold for each sample.
@@ -33,11 +28,6 @@ class DPSGDFOptimizer(DPOptimizer):
     4) Add Gaussian noise to ``p.grad`` calibrated to a given noise multiplier and
     adaptive clipping thresholds.
     5) Call underlying optimizer to perform optimization step.
-
-    References:
-        [1] Xu D, Du W, Wu X. Removing disparate impact of differentially private
-        stochastic gradient descent on model accuracy.
-        arXiv preprint arXiv:200303699. 2020.
     """
 
     def __init__(
@@ -46,8 +36,8 @@ class DPSGDFOptimizer(DPOptimizer):
         *,
         noise_multiplier: float,
         base_clipping_threshold: float,
-        expected_batch_size: Optional[int],
         n_groups: int,
+        expected_batch_size: Optional[int],
         loss_reduction: str = "mean",
         generator: torch.Generator = None,
         secure_mode: bool = False,
@@ -61,13 +51,13 @@ class DPSGDFOptimizer(DPOptimizer):
                 The noise multiplier.
             base_clipping_threshold (float):
                 The base clipping threshold used to clip the norm of the gradients.
+            n_groups (int):
+                The number of protected subgroups in the data.
             expected_batch_size (Optional[int]):
                 The batch_size used for averaging gradients. When using
                 Poisson sampling averaging denominator can't be inferred from the
                 actual batch size. Required if ``loss_reduction="mean"``, ignored if
                 ``loss_reduction="sum"``.
-            n_groups (int):
-                The number of sensitive subgroups in the data.
             loss_reduction (str, optional):
                 Indicates if the loss reduction (for aggregating the gradients)
                 is a sum or a mean operation. Can take values "sum" or "mean".
@@ -81,8 +71,9 @@ class DPSGDFOptimizer(DPOptimizer):
                 See ``opacus.optimizers.optimizer._generate_noise`` for details.
                 Defaults to False.
             log_thresholds (bool, optional):
-                Logs the clipping thresholds at each iteration, storing them in class
-                variable ``self.thresholds``. Defaults to False.
+                Logs the clipping mean group thresholds at each iteration,
+                storing them in class variable ``self.logged_thresholds``.
+                Defaults to False.
         """
 
         super().__init__(
@@ -95,18 +86,18 @@ class DPSGDFOptimizer(DPOptimizer):
             secure_mode=secure_mode,
         )
 
-        self.C0 = base_clipping_threshold
+        self.base_clip = base_clipping_threshold
         self.n_groups = n_groups
 
-        self.sample_group_labels = None
+        self.batch_params: Dict[str, Any] = {}
         self.log_thresholds = log_thresholds
 
         if self.log_thresholds:
-            self.thresholds: List[List[float]] = [[] for i in range(n_groups)]
+            self.logged_thresholds: List[List[float]] = [[] for i in range(n_groups)]
 
     def clip_and_accumulate(self):
         """Performs gradient clipping and accumulation.
-        Stores clipped and aggregated gradients into `p.summed_grad```.
+        Stores clipped and aggregated gradients into ``p.summed_grad``.
         """
 
         per_param_norms = [
@@ -114,8 +105,14 @@ class DPSGDFOptimizer(DPOptimizer):
         ]
         per_sample_norms = torch.stack(per_param_norms, dim=1).norm(2, dim=1)
 
+        # Modified from parent
+
         C = self.compute_clipping_bounds(per_sample_norms)
+        self.max_grad_norm = C.max()
+
         per_sample_clip_factor = (C / (per_sample_norms + 1e-6)).clamp(max=1.0)
+
+        #
 
         for p in self.params:
             _check_processed_flag(p.grad_sample)
@@ -130,8 +127,11 @@ class DPSGDFOptimizer(DPOptimizer):
 
             _mark_as_processed(p.grad_sample)
 
-    def compute_clipping_bounds(self, per_sample_norms: torch.Tensor) -> torch.Tensor:
-        """Computes the variable clipping bounds for each sample.
+    @abstractmethod
+    def compute_clipping_bounds(
+        self, per_sample_norms: torch.Tensor, **kwargs
+    ) -> torch.Tensor:
+        """Computes the variable clipping bounds for each sample in the batch.
 
         Args:
             per_sample_norms (torch.Tensor):
@@ -142,21 +142,64 @@ class DPSGDFOptimizer(DPOptimizer):
             torch.Tensor:
                 The clipping bounds of shape (batch_size,).
         """
+        pass
 
-        if self.sample_group_labels is None:
+    def set_batch_params(self, **kwargs):
+        """Sets the batch parameters used by ``compute_clipping_bounds``.
+
+        Args:
+            **kwargs:
+                By default any key word arguments passed to this function
+                will be stored in the ``batch_params`` dictionary and
+                passed to ``compute_clipping_bounds``.
+        """
+
+        self.batch_params = kwargs
+
+    def _log_clipping_thresholds(self, C, group_labels):
+        if not self.log_thresholds:
+            return
+
+        for k in range(self.n_groups):
+            group_mask = group_labels == k
+            if torch.sum(group_mask) == 0:
+                self.logged_thresholds[k].append(self.base_clip)
+                continue
+
+            threshold = torch.mean(C[group_mask]).detach().cpu().numpy().squeeze()
+            self.logged_thresholds[k].append(float(threshold))
+
+
+class DPSGDFOptimizer(AdaptiveClippingOptimizer):
+    """
+    Implementation of DPSGD-F from Xu et al. [1].
+
+    References:
+        [1] Xu D, Du W, Wu X. Removing disparate impact of differentially private
+        stochastic gradient descent on model accuracy.
+        arXiv preprint arXiv:200303699. 2020.
+    """
+
+    def compute_clipping_bounds(
+        self, per_sample_norms: torch.Tensor, **kwargs
+    ) -> torch.Tensor:
+        if "group_labels" not in kwargs:
             raise ValueError(
-                "sample_group_labels has not been cached. Pass the group labels to \
-                ``DPSGDFOptimizer.update_cached_params`` before calling \
-                DPSGDFOptimizer.step"
+                "``group_labels`` has not been set. Pass the batch group labels \
+                to ``DPSGDFOptimizer.set_batch_params()`` before calling \
+                ``DPSGDFOptimizer.step()``."
             )
 
-        n_samples = len(self.sample_group_labels)
-        C = torch.full((n_samples,), self.C0).to(device)
+        batch_group_labels = kwargs["group_labels"]
+        assert len(batch_group_labels) == len(per_sample_norms)
 
-        m_mask = per_sample_norms > self.C0
+        n_samples = len(batch_group_labels)
+        C = torch.full((n_samples,), self.base_clip).to(device)
+
+        m_mask = per_sample_norms > self.base_clip
         m_sum = 0
         for k in range(self.n_groups):
-            group_mask = self.sample_group_labels == k
+            group_mask = batch_group_labels == k
             count = torch.sum(group_mask)
             if count == 0:
                 continue
@@ -164,25 +207,12 @@ class DPSGDFOptimizer(DPOptimizer):
             m = torch.sum(group_mask & m_mask)
             m_ratio = m / count
 
-            C[group_mask] = self.C0 * (1 + n_samples * m_ratio)
+            C[group_mask] = self.base_clip * (1 + n_samples * m_ratio)
 
             m_sum += m
 
         C /= m_sum
 
-        self.max_grad_norm = C.max()
-
-        if self.log_thresholds:
-            for k in range(self.n_groups):
-                group_mask = self.sample_group_labels == k
-                if torch.sum(group_mask) == 0:
-                    self.thresholds[k].append(self.C0)
-                    continue
-
-                threshold = torch.mean(C[group_mask]).detach().cpu().numpy().squeeze()
-                self.thresholds[k].append(float(threshold))
+        self._log_clipping_thresholds(C, batch_group_labels)
 
         return C
-
-    def set_batch_params(self, *, group_labels: torch.Tensor):
-        self.sample_group_labels = group_labels
